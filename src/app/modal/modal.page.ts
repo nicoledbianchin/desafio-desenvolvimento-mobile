4d4e8a5c3de93a34409643e5d08e7ffe9eb56d2d
nicoledbianchin <nicoledbianchin@gmail.com>
2023-09-12 21:07:53 -0300

    adiciona markers

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.page.scss'],
 })
 export class ModalPage implements OnInit {
+  @Input() marker: any;
 
   constructor() { }
 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps'
+import { GoogleMap, Marker } from '@capacitor/google-maps'
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +11,7 @@ import { GoogleMap } from '@capacitor/google-maps'
 export class Tab2Page {
   @ViewChild('map')mapRef!: ElementRef;
   map!: GoogleMap;
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
 
   ionViewDidEnter() {
     this.createMap();
@@ -23,11 +25,37 @@ export class Tab2Page {
       forceCreate: true,
       config: {
         center: {
-          lat: 33.6,
-          lng: -117.9
+          lat: -29.695666114544306,
+          lng: -51.48097442650094
         },
-        zoom: 8
+        zoom: 16,
       }
+    })
+    await this.addMarkers();
+  }
+
+  async addMarkers() {
+    const markers: Marker[] = [
+      {
+        coordinate: {
+          lat: -29.695666114544306,
+          lng: -51.48097442650094
+        },
+        title: 'TATS Comunicação Visual',
+        snippet: 'Empresa'
+      }
+    ]
+    
+    const result = await this.map.addMarkers(markers);
+
+    this.map.setOnMarkerClickListener(async(marker) => {
+      const modal = await this.modalCtrl.create({
+        component: ModalPage,
+        componentProps: { marker },
+        breakpoints: [0, 0.3],
+        initialBreakpoint: 0.3
+      });
+      modal.present();
     })
   }
 }
